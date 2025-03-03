a162c96044e8c960e7eef756da8aeda5048e369f
giovanni-arduini <gionnibigudd@gmail.com>
2025-03-03 09:38:35 +0100

    ADD bonuses

--- ex.js
+++ ex.js
@@ -6,21 +6,29 @@ async function fetchJson(url){
 
 async function getDashboardData(city){
 
-    const destinationPromise =  (await fetchJson(`https://boolean-spec-frontend.vercel.app/freetestapi/destinations?search=${city}`))
- 
-    const weathersPromise =  (await fetchJson(`https://boolean-spec-frontend.vercel.app/freetestapi/weathers?search=${city}`))
-
-    const airportsPromise =  (await fetchJson(`https://boolean-spec-frontend.vercel.app/freetestapi/airports?search=${city}`)) 
-
-    const promises = [destinationPromise, weathersPromise, airportsPromise]
-    const [destinations, weathers, airports] = await Promise.all(promises)
-
-    return {
-        city: destinations[0].name,
-        country: destinations[0].country,
-        temperature: weathers[0].temperature,
-        weather: weathers[0].weather_description,
-        airport: airports[0].name
+    try{
+        const destinationPromise =  (await fetchJson(`https://boolean-spec-frontend.vercel.app/freetestapi/destinations?search=${city}`))
+     
+        const weathersPromise =  (await fetchJson(`https://boolean-spec-frontend.vercel.app/freetestapi/weathers?search=${city}`))
+    
+        const airportsPromise =  (await fetchJson(`https://boolean-spec-frontend.vercel.app/freetestapi/airports?search=${city}`)) 
+    
+        const promises = [destinationPromise, weathersPromise, airportsPromise]
+        const [destinations, weathers, airports] = await Promise.all(promises)
+        
+        const destination = destinations[0];
+        const weather = weathers[0];
+        const airport = airports[0];
+        
+        return {
+            city: destination ? destinations[0].name : null,
+            country: destination ? destinations[0].country : null,
+            temperature: weather ? weathers[0].temperature : null,
+            weather: weather ? weathers[0].weather_description : null,
+            airport: airport ? airports[0].name : null
+        }
+    }catch(error){
+        console.error(error)
     }
 
 }
@@ -36,14 +44,18 @@ async function getDashboardData(city){
 // })()
 
 
-
-getDashboardData('london')
+getDashboardData('milan')
     .then(data => {
-        console.log('Dasboard data:', data);
-        console.log(
-            `${data.city} is in ${data.country}.\n` +
-            `Today there are ${data.temperature} degrees and the weather is ${data.weather}.\n`+
+        let message = ""
+
+        if(data.city !== null && data.country !== null){
+            message += `${data.city} is in ${data.country}.\n`
+        } if (data. temperature !== null && data.weather !== null){
+             message += `Today there are ${data.temperature} degrees and the weather is ${data.weather}.\n`
+        } if (data.airport !== null){
             `The main airport is ${data.airport}.\n`
-        );
+        } if(!data.city&&!data.country&&!data.temperature&&!data.weather&&!data.airport){message = "No available information for the selected destination"}
+
+        console.log(message);
     })
     .catch(error => console.error(error));
